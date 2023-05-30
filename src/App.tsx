57ba31ea2c8cdb86e1a3704b089a502e4cf52d55
m k <kadrimalek103@gmail.com>
2023-05-30 12:28:42 +0200

    pull

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,8 @@ import { Details } from "./pages/details/Details.tsx";
 import React, { useEffect, useState } from "react";
 
 import "./App.css";
-import { Header } from "./shared/Header";
-import { Filters } from "./pages/homepage/components/Filters";
-import { CardsList } from "./pages/homepage/components/CardsList";
-import { getData } from "./api/data";
-import { Categories } from "./models/Categories.ts";
-import { MoviesList } from "./models/MoviesList";
-import { Movie } from "./models/Movie.ts";
-import { getMovieById } from "./api/Movie.ts";
-import { Link, Outlet, Route, Routes } from "react-router-dom";
+
+import { Route, Routes } from "react-router-dom";
 import { Homepage } from "./pages/homepage/HomePage.tsx";
 const App = () => {
   return (
