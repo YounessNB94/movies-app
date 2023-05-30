cc7f226f9f1bea4348d865a76382a2a81fa2a0c3
m k <kadrimalek103@gmail.com>
2023-05-30 14:08:11 +0200

    newStuff

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,12 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 
 import { Route, Routes } from "react-router-dom";
-import { Homepage } from "./pages/homepage/HomePage.tsx";
+import { HomePage } from "./pages/homepage/HomePage.tsx";
 const App = () => {
   return (
     <div className="main">
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/details" element={<Details />} />
       </Routes>
     </div>
